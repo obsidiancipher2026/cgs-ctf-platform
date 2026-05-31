cec55a452b37e784449c926ea1a0e650300a3376
obsidiancipher2026 <obsidiancipher2026@gmail.com>
2026-05-31 22:07:03 +0500

    fix: resolve merge conflicts in .gitignore and next.config.js

--- next.config.js
+++ next.config.js
@@ -5,12 +5,6 @@ const nextConfig = {
   images: {
     domains: [],
   },
-<<<<<<< HEAD
-=======
-  experimental: {
-    serverComponentsExternalPackages: ['argon2'],
-  },
->>>>>>> 3faa2baf685d32f0a41b1e08f244efdb5a8ea6e6
 }
 
 module.exports = nextConfig
